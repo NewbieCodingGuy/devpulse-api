b86de2a0d8de84eb13de8122013e9ad38e796732
NewbieCodingGuy <shekharali302@gmail.com>
2026-05-24 12:07:30 +0530

    Add rate limiting and apply to auth routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { userRoutes } from "./modules/user/user.routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { sessionRoutes } from "./modules/session/session.routes";
+import { globalLimiter } from "./middlewares/rateLimiter";
 
 const app = express();
 app.use(express.json());
+app.use(globalLimiter);
 
 // routes
 app.use("/api/auth", userRoutes);
