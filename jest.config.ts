6c69fa872fd9179997511cee2624f403a8a8a8df
NewbieCodingGuy <shekharali302@gmail.com>
2026-06-02 19:29:14 +0530

    Add Jest config, tests, and test env

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  roots: ["<rootDir>/src"],
+
+  testMatch: ["**/*.test.ts"],
+
+  moduleFileExtensions: ["ts", "js"],
+
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],
+
+  clearMocks: true,
+};
